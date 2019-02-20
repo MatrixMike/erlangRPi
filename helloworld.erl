5c39bb5be3edf38055a751d8e3c865c6cf5420e1
Mike <matroxmike@icloud.com>
2019-02-20 21:20:46 +1100

    for RPi version

--- helloworld.erl
+++ helloworld.erl
@@ -5,6 +5,6 @@ calculate(0) -> 1;
 calculate(N) -> N * calculate(N -1).
 % a comment
 start() ->
-    io:fwrite("calc=", calculate(4)).
-%    io:fwrite("hello Mike 2\n").
+    io:fwrite("calc=", calculate(4)),
+    io:fwrite("hello Mike 2\n").
 
