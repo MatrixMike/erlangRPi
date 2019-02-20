93ece4eb4815f749eb284ab387a844e77c4b3cef
Mike <matroxmike@icloud.com>
2019-02-20 22:54:44 +1100

    internal documentation

--- list_comp.erl
+++ list_comp.erl
@@ -1,18 +1,15 @@
 -module(list_comp).
 -compile(export_all).
 
-
+% compile this, in the REPL, with (list_comp).
+% execute with list_comp:run().
+% note fullstop at end of line
 run() ->
     L = [1,2,3,4,5],
-
-
-
 
     LL = [X*X || X <- L],
     io:format("~p~n", [L]),
     io:format("~p~n", [LL]),
-
-
 
     Evens = [X || X <- L, X rem 2 == 0],
     io:format("~p~n", [Evens]).
