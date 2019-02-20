ff13c733723765cf3db12f24714782e315724bf6
Mike <matroxmike@icloud.com>
2019-02-20 21:33:34 +1100

    two IO

--- helloworld.erl
+++ helloworld.erl
@@ -5,6 +5,7 @@ calculate(0) -> 1;
 calculate(N) -> N * calculate(N -1).
 % a comment
 start() ->
-    io:fwrite("calc=", calculate(4)),
+%    io:fwrite("calc=", calculate(4)),
+    io:fwrite("hello Mike 1\n"),
     io:fwrite("hello Mike 2\n").
 
