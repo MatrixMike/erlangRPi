7feb7b2c51901ff396364c5916e95d52564e1c50
Mike <matroxmike@icloud.com>
2019-02-21 21:38:10 +1100

    fwrite needs []

--- helloworld.erl
+++ helloworld.erl
@@ -2,10 +2,10 @@
 %-compile(export_all).
 -export([start/0]).
 calculate(0) -> 1;
-calculate(N) -> N * calculate(N -1).
+calculate(N) -> N * calculate(N-1).
 % a comment
 start() ->
-%    io:fwrite("calc=", calculate(4)),
+    io:fwrite("calc= ~p\n", [calculate(4)]),
     io:fwrite("hello Mike 1\n"),
     io:fwrite("hello Mike 2\n").
 
