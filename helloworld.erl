01fa39eb4dc670f97c894bedb69b4c15b589d97a
Mike <matroxmike@icloud.com>
2019-02-22 12:54:22 +1100

    changed arguments to test their usage

--- helloworld.erl
+++ helloworld.erl
@@ -1,10 +1,10 @@
 -module(helloworld).
 %-compile(export_all).
--export([start/0]).
+-export([start1/0]).
 calculate(0) -> 1;
 calculate(N) -> N * calculate(N-1).
 % a comment
-start() ->
+start1() ->
     io:fwrite("calc= ~p\n", [calculate(4)]),
     io:fwrite("hello Mike 1\n"),
     io:fwrite("hello Mike 2\n").
--- testERLANG.sh
+++ testERLANG.sh
@@ -3,5 +3,6 @@ erl -version
 erlc helloworld.erl
 erlc calculate1.erl
 # % is a comment in erlang 
-erl -noshell -s helloworld start -s init stop 
+# http://erlang.org/documentation/doc-6.1/erts-6.1/doc/html/erl.html
+erl -noshell -s helloworld start1 -s init stop 
 #
