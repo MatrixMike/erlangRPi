73d64ca009210827d801547b81b7f238b37f27b0
Mike <matroxmike@icloud.com>
2019-02-22 13:09:02 +1100

    command line options

--- testERLANG.sh
+++ testERLANG.sh
@@ -5,4 +5,7 @@ erlc calculate1.erl
 # % is a comment in erlang 
 # http://erlang.org/documentation/doc-6.1/erts-6.1/doc/html/erl.html
 erl -noshell -s helloworld start1 -s init stop 
-#
+erl -s helloworld start1 -s init stop -noshell
+erl -s helloworld start1 -s init stop -noshell | wc
+
+
